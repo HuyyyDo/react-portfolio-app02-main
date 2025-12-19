7bbbf3c8b8e0968f548a6b66c7a6168b7bf520d3
Trong Do Huy Hoang <huyhoang.ais@gmail.com>
2025-12-18 22:13:50 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer(){
     <div style={{display:'flex', justifyContent:'space-between', padding:'1rem 0'}}>
       <span>© {year} Trong Do Huy Hoang</span>
       <span className="small">
-        <a href="https://github.com/HuyyyDo2/react-portfolio-app02" target="_blank" rel="noreferrer">GitHub</a> ·{' '}
+        <a href="https://github.com/HuyyyDo" target="_blank" rel="noreferrer">GitHub</a> ·{' '}
       </span>
     </div>
   )
